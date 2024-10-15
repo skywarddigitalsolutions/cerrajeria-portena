3bf1cf269672ba58b841bd49cb8e030324f669d2
Agustin <agusquiroga648@gmail.com>
2024-10-15 19:21:31 -0300

    cambio tamaño navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
   
   return (
     <div
-      className='bg-celeste shadow-md'
+      className='fixed bg-celeste shadow-md'
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-24">
